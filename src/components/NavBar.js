4298d27626707f9e762af93fd993326978fa51e6
Sophia Chipeco <sgc6@sfu.ca>
2025-03-09 08:47:22 -0700

    nav profile stuff

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './NavBar.css';
 
 const NavBar = () => {
-    const navLinks = [
-        { name: 'Home', path: '/' },
-        { name: 'Profile', path: '/profile' },
-        { name: 'LogIn', path: '/logIn' },
-        { name: 'ChatBot', path: '/ChatBot' }
-    ];
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Default to false
+        const [user, setUser] = useState({}); // Default to an empty object
+    
+        useEffect(() => {
+            // Check if the user data is in localStorage
+            const user = JSON.parse(localStorage.getItem('user'));
+    
+            // If user exists, set isLoggedIn to true
+            if (user) {
+                setIsLoggedIn(true);
+                setUser(user);
+            } else {
+                setIsLoggedIn(false);
+                setUser(null); // If user does not exist, set isLoggedIn to false
+            }
+        }, []);
+
+        const navLinks = [
+            { name: 'Home', path: '/' },
+             ...(isLoggedIn ? [{ name: 'Profile', path: '/profile' }] : []),
+            { name: 'ChatBot', path: '/ChatBot' }
+        ];
+
+         const handleLogout = () => {
+        localStorage.removeItem('user');
+        setIsLoggedIn(false);
+        setUser(null);
+    };
+
+
     return (
         <nav className="navbar">
         <ul className="nav-links">
