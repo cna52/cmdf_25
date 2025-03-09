6502d268d459ba003011368366f7d7bf7fd6b65a
Camille <cna52@sfu.ca>
2025-03-08 22:50:45 -0800

    added styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>This is the home page of our application.</p>
+            <div class="sideBar">
+                <div class="sideBarContent">
+                    <h1>Profile</h1>
+                    <p>Welcome to the user profile page.</p>
+                    <button variant="primary" onClick={() => window.location.href='/LogIn'}>LogIn</button>
+                    {/* <button variant="secondary" onClick={() => window.location.href='/profile'}>Profile</button>
+                    <button variant="success" onClick={() => window.location.href='/settings'}>Settings</button> */}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/LogIn.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LogIn.css';;
+
+const LogIn = () => {
+    return (
+        <div>
+            <div class="card">
+                <div class="cardContent">
+                    <h1>Log In</h1>
+                    <p>Welcome to the user profile page.</p>
+                    <button>Google</button>
+                </div>    
+            </div>
+        </div>
+    );
+};
+
+export default LogIn;
